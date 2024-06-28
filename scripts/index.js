d221389b0aaadffd18fca3154191105c6c4757fc
randomduckduck <randomduckduck@duck.com>
2024-06-28 22:09:06 +0530

    found out Babel does not convert files inside files, so we cant import from other js files (which means everything will have to be in same file). Tries lots of cdn packages but babel only transforms one js./jsx file and not imports of other file inside js/jsx file.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,64 @@
-function Main() {
-  return <div>Something</div>;
+// const { BrowserRouter } = require("./react-router-dom.development");
+// let mm = ReactRouterDOM.createBrowserRouter(
+//   [
+//     {
+//       path: "/",
+//       element: <div>jyvutvhv</div>,
+//     },
+//   ],
+//   {
+//     basename: "/app",
+//   }
+// );
+// let { Route, Switch, Link } = ReactRouterDOM;
+
+// function Main() {
+//   return <div>Something</div>;
+// }
+// // const Link = ReactRouterDOM.Link;
+// // const Route = ReactRouterDOM.Route;
+
+// function Main2() {
+//   return (
+//     <>
+//       <ReactRouterDOM.RouterProvider router={mm} fallbackElement={<Main />}>
+//         <div>Something</div>
+//       </ReactRouterDOM.RouterProvider>
+//     </>
+//   );
+// }
+// // <BrowserRouter>
+// {
+//   /* <ReactRouterDOM.HashRouter> */
+// }
+// {
+//   /* <ul>
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/login">Login</Link>
+//         </li>
+//         <li>
+//           <Link to="/register">Register</Link>
+//         </li>
+//       </ul>
+
+//       <Route path="/" exact component={Home} />
+//       <Route path="/login" component={Login} />
+//       <Route path="/register" component={Register} /> */
+// }
+// {
+//   /* </ReactRouterDOM.HashRouter> */
+// }
+// // </BrowserRouter>
+// const Home = () => <h1>Home</h1>;
+// const Login = () => <h1>Login</h1>;
+// const Register = () => <h1>Register</h1>;
+
+function Main3() {
+  return <div>Helloaskda</div>;
 }
+
 var kk = ReactDOM.createRoot(document.querySelector("#root"));
-kk.render(<Main />);
+kk.render(<Main3 />);
